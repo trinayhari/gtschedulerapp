87382c38ba8f2c4cdb2b53b04492fc336812f8ed
trinayhari <trinayhari@gmail.com>
2024-01-24 19:27:44 -0500

    initial commit

--- app/src/main/java/com/example/gtschedulerapp/MainActivity.java
+++ app/src/main/java/com/example/gtschedulerapp/MainActivity.java
@@ -1,6 +1,14 @@
 package com.example.gtschedulerapp;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -12,7 +20,14 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.gtschedulerapp.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView listView;
+    private Button button;
 
     private ActivityMainBinding binding;
 
@@ -32,6 +47,62 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        listView = findViewById(R.id.listView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addItem(view);
+            }
+        });
+
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,items);
+        listView.setAdapter(itemsAdapter);
+        setUpListViewListener();
+
     }
 
+    private void setUpListViewListener() {
+        // Ensure correct context for Toast
+        Context context = this; // Use 'this' for Activity context
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+
+
+            public
+
+            boolean
+
+            onItemLongClick(AdapterView<?> adapterView, View view, int i, long l)
+
+            {
+                Toast.makeText(context, "Item Removed", Toast.LENGTH_LONG).show();
+
+                // Remove the item from the backing data source
+                items.remove(i);
+
+                // Notify the adapter of data changes
+                itemsAdapter.notifyDataSetChanged();
+
+                return true; // Indicate long click handled
+            }
+        });
+    }
+
+    private void addItem(View view) {
+        EditText input = findViewById(R.id.);
+        String itemText = input.getText().toString();
+
+        if(!(itemText.equals(""))){
+            itemsAdapter.add(itemText);
+            input.setText("");
+        }
+        else {
+            Toast.makeText(getApplicationContext(),"Please enter text",Toast.LENGTH_LONG).show();
+        }
+    }
 }
